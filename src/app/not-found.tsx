c100b3477062a80eaded5efb76ad467565e76c21
Rajmalla1 <raj.malla@slcc.edu.np>
2024-05-02 11:29:17 +0545

    Revert "feat: 404 error page added"
    This reverts commit c8d3158cd24718016572a4a806544cfd3ec89bec.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,10 +9,10 @@ const NotFoundPage: React.FC = () => {
 
     return (
         <div className="min-h-screen flex items-center justify-center">
-            <div className="max-w-screen-lg w-full px-4 sm:px-8 md:px-16"> 
-                <h1 className="text-3xl font-bold mb-8 text-gray-700">Oh No!!</h1>
+            <div className="max-w-screen-lg w-full px-4 sm:px-8 md:px-16"> {/* Adjusted padding for different screen sizes */}
+                <h1 className="text-3xl font-bold mb-8 text-gray-700">Oh No!!</h1> {/* Increased margin bottom */}
                 <p className="text-3xl mb-6 font-extrabold text-slate-900">Error <span className="text-3xl text-gray-300 bg-opacity-50 px-2">404</span></p> {/* Increased margin bottom */}
-                <div className="mb-8"> 
+                <div className="mb-8"> {/* Increased margin bottom */}
                     <p className="text-3xl font-extrabold text-green-500">Page Not found</p>
                 </div>
                 <p className="pb-4 pt-6 text-slate-600">Oops! Looks like you've stumbled upon a page that doesn't exist. But don't worry, <br /> <span>We're here to help you get back on track</span></p>
