c549458cd265d8c6d2206d7f4059cf35ff7f8023
Rajmalla1 <raj.malla@slcc.edu.np>
2024-05-14 17:39:39 +0545

    fix(resposive) Fixed responsive issue for below small screen

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -40,9 +40,23 @@ className="object-cover brightness-75"
 </div>
 
 <div className='w-full md:w-1/2 h-full flex flex-col p-10 text-white items-center md:items-start'>
-<h1 className='text-2xl md:text-4xl text-green-500 font-extrabold mt-20 pt-20'>Tat Tato</h1>
-<h2 className='text-slate-600 pt-4 text-3xl font-bold'>Welcome Back!</h2>
-<p className='text-slate-600 pt-2'>Don't have an account?<Link className='text-green-500 ml-2 font-bold hover:text-green-600' href={'/signup'}>SignUp</Link></p>
+
+
+<div className='lg:-ml-4 -mb-7 '>
+<Image
+src="/images/logo-Photoroom.png-Photoroom.png"
+alt="Logo"
+width={100}
+height={50}
+/>
+</div>
+
+
+<h2 className='text-slate-600 pt-4 text-md md:text-3xl font-bold'>Welcome Back!</h2>
+
+
+<p className='text-slate-600 pt-2 text-sm md:text-'>Don't have an account?<Link className='text-green-500 ml-2 font-bold hover:text-green-600' href={'/signup'}>SignUp</Link></p>
+
 
 <label htmlFor="email" className="block mb-1 text-sm font-medium text-slate-400 mt-8">
 Email
@@ -50,11 +64,12 @@ Email
 <input
 type="text"
 id="email"
-className="bg-white border-2 border-slate-400 text-gray-900 text-sm rounded-lg focus:border-slate-500 block w-3/4 p-3"
+className="bg-white border-2 border-slate-400 text-gray-900 text-sm rounded-lg focus:border-slate-500 block w-full md:w-3/4 p-3" // Changed width for small screens
 placeholder="Email"
 required
 onChange={(e) => setUser({ ...user, email: e.target.value })}
 />
+
 
 <label htmlFor="password" className="block mb-1 text-sm font-medium text-slate-400 mt-4">
 Password
@@ -62,40 +77,45 @@ Password
 <input
 type="password"
 id="password"
-className="bg-white border-2 border-slate-400 text-gray-900 text-sm rounded-lg focus:border-slate-500 block w-3/4 p-3"
+className="bg-white border-2 border-slate-400 text-gray-900 text-sm rounded-lg focus:border-slate-500 block w-full md:w-3/4 p-3" // Changed width for small screens
 placeholder="Password"
 required
 onChange={(e) => setUser({ ...user, password: e.target.value })}
 />
 
-<div className='flex flex-row items-center md:justify-between pt-5 w-3/4'>
+
+<div className='flex flex-row items-center sm:justify-between pt-5 w-full md:w-3/4'>
 <div className="flex items-center">
 <input
 type="checkbox"
 id="agree-terms"
 value="agree"
-className="w-5 h-5 pr-1 rounded bg-gray-100 border-gray-300 focus:border-2 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+className="md:w-5 md:h-5 w-3 h-3 pr-1 rounded bg-gray-100 border-gray-300 focus:border-2 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
 />
-<p className='text-slate-600 ml-2'>Remember Me</p>
+<p className='text-slate-600 text-xs md:text-base ml-2'>Remember Me</p>
 </div>
-<Link href={'/forgotpassword'} className='text-green-500 hover:underline hover:text-green-700'>Forgot Password?</Link>
+<Link href={'/forgotpassword'} className='text-green-500 hover:underline hover:text-green-700 text-xs md:text-base ml-3 sm:ml-0'>Forgot Password?</Link>
 </div>
+
 
 <CustomButton
 title="Login"
-containerStyles="bg-green-500 border text-white rounded-lg mt-4 w-3/4 h-12 hover:bg-green-600 hover:text-white pt-2 "
+containerStyles="bg-green-500 border text-white rounded-lg mt-4 w-full md:w-3/4 h-12 hover:bg-green-600 hover:text-white pt-2" // Changed width for small screens
 handleClick={handleClick}
 />
 
-<p className='text-slate-600 pt-2 '>Don't have an Account? <Link className='text-green-500 ml-2 font-bold hover:text-green-600 hover:underline' href={'/forgotPassword'}>Signup</Link></p>
 
-<div className='flex flex-row items-center mt-4 md:mt-auto md:flex-row md:items-center'>
-<h1 className='text-slate-500 text-sm md:mr-4 cursor-pointer hover:underline font-bold' onClick={() => { onLogin }}>Need Help?</h1>
-<Link href="mailto:hello@gmail.com" className='text-green-500 text-md cursor-pointer hover:underline font-bold '>hello@gmail.com</Link>
-<Link href="/terms" className='text-slate-500 text-sm ml-2 md:ml-4 cursor-pointer hover:underline font-bold'>Terms & Conditions</Link>
+<p className='text-slate-600 pt-2 text-sm md:text-base'>Don't have an Account? <Link className='text-green-500 ml-2 font-bold hover:text-green-600 hover:underline text-sm md:text-base ' href={'/forgotPassword'}>Signup</Link></p>
+
+
+<div className='flex flex-row md:flex-row items-center justify-center md:justify-center mt-4 md:pt-10 md:-ml-12 lg:ml-0 md:items-center flex-wrap md:mt-auto '>
+<h1 className='text-slate-500 text-xs md:text-sm md:mr-4 cursor-pointer hover:underline font-bold' onClick={() => {onLogin}}>Having issue?</h1>
+<Link href="mailto:hello@gmail.com" className='text-green-500 text-xs md:text-sm cursor-pointer pl-2 hover:underline font-bold '>hello@gmail.com</Link>
+<Link href="/terms" className='text-slate-500 text-xs md:text-sm ml-2 md:ml-4 cursor-pointer hover:underline font-semibold'>Terms & Conditions</Link>
 </div>
 </div>
 </section>
+
 );
 };
 
