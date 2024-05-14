f97cec5c1dfa3b28fe963c12e1fb30a5196cc481
Rajmalla1 <raj.malla@slcc.edu.np>
2024-05-14 18:15:03 +0545

    fix(wording issue) Removed 403 from maintenance page

--- src/app/maintenance.tsx
+++ src/app/maintenance.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import CustomButton from './components/CustomButton';
 
-const NotFoundPage: React.FC = () => {
+const MaintenancePage: React.FC = () => {
     function handleClick(): void {
 
     }
@@ -10,16 +10,17 @@ const NotFoundPage: React.FC = () => {
     return (
         <div className="min-h-screen flex items-center justify-center">
             <div className="max-w-screen-lg w-full px-4 sm:px-8 md:px-16"> 
-                <h1 className="text-3xl font-bold mb-8 text-gray-700">Oh No!!</h1> 
-                <p className="text-3xl mb-6 font-extrabold text-slate-900">Sorry!!<span className="text-3xl text-gray-300 bg-opacity-50 px-2">403</span></p> {/* Increased margin bottom */}
-                <div className="mb-8">
-                    <p className="text-3xl font-extrabold text-green-500">Site under maintenance</p>
+                <h1 className="text-base md:text-2xl font-bold mb-8 text-gray-700">Oh No!!</h1> 
+                <p className=" text-xl md:text-4xl sm:mb-6 font-extrabold text-slate-900">Sorry!!</p> 
+                <div className="sm:mb-4">
+                    <p className="text-xl  md:text-4xl font-extrabold text-green-500">Site under maintenance</p>
                 </div>
-                <p className="pb-4 pt-6 text-slate-600">We're currently undergoing maintenance to improve your experience. <br /> <span> Please bear with us while we work to make things better.</span></p>
+                <p className="text-xs sm:text-base  pb-7 pt-4 mb-6 text-slate-600">We're currently undergoing maintenance to improve your experience. <br /> <span> Please bear with us while we work to make things better.</span></p>
+
 
                 <CustomButton
                     title="Go back to home page"
-                    containerStyles="bg-green-500 border text-white rounded-lg py-3 px-5"
+                    containerStyles="bg-green-500 border text-white rounded-lg py-3 px-5 "
                     handleClick={handleClick}
                 />
             </div>
@@ -27,4 +28,4 @@ const NotFoundPage: React.FC = () => {
     );
 };
 
-export default NotFoundPage;
+export default MaintenancePage;
